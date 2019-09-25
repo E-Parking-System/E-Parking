cb5d4b75b3f560c37d2647a6f104b9101f044779
MirzaAhmedBaig <mirzaahmed65@gmail.com>
2019-09-25 11:54:11 +0530

    project config

---
+++ app/src/main/java/com/km/eparkinguser/SplashScreen.java
@@ -0,0 +1,19 @@
+package com.km.eparkinguser;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Created by Mirza Ahmed Baig on 2019-09-25.
+ * Avantari Technologies
+ * mirza@avantari.org
+ */
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
